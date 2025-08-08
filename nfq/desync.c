a40bd6c0db5b5b7268c91569fc4de810b867cf22
ivsky-cpu <backafter6monthscrashed@gmail.com>
2025-08-08 22:52:08 +0300

    Update desync.c

--- nfq/desync.c
+++ nfq/desync.c
@@ -1998,8 +1998,10 @@ static uint8_t dpi_desync_tcp_packet_play(bool replay, size_t reasm_offset, uint
 									seg, seg_len, pkt1, &pkt1_len))
 								goto send_orig;
 							ip_id=IP4_IP_ID_NEXT(ip_id);
-							DLOG("sending multisplit part %d %zu-%zu len=%zu seqovl=%u : ",i+1,from,to-1,to-from,seqovl);
+							DLOG("sending multisplit part %d %zu-%zu len=%zu seqovl=%u msplit_count=%d: ",i+1,from,to-1,to-from,seqovl,multisplit_count);
 							hexdump_limited_dlog(seg,seg_len,PKTDATA_MAXDUMP); DLOG("\n");
+							usleep(500000);
+							DLOG("delayed send.");
 							if (!rawsend((struct sockaddr *)&dst, desync_fwmark, ifout , pkt1, pkt1_len))
 							{
 #ifdef __linux__
@@ -2878,7 +2880,6 @@ static uint8_t dpi_desync_udp_packet_play(bool replay, size_t reasm_offset, uint
 						if (!rawsend_rep(dp->desync_repeats,(struct sockaddr *)&dst, desync_fwmark, ifout , pkt1, pkt1_len))
 							goto send_orig;
 						ip_id=IP4_IP_ID_NEXT(ip_id);
-						usleep(500000);
 					}
 					bFake = true;
 				}
